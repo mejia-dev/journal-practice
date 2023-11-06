63e6f67d83b43269207ba82052df7d2bd1705c04
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-06 09:39:41 -0800

    Initial project setup commit. No linter yet

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devServer: {
+		static: {
+    directory: path.join(__dirname, "dist"),
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      verbose: true
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Journal Practice',
+      template: './src/index.html',
+      inject: 'body'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  }
+};
